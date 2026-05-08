ba0761d20efc62bb2f9344de1de1c6357d29ce11
Daniel Santiago <daniel.losada50667@ucaldas.edu.co>
2026-05-08 09:24:14 -0500

    feat: nuevo archivo Condicionales

---
+++ src/Condicionales.java
@@ -0,0 +1,11 @@
+public class Condicionales {
+    static int numeroUno = 10;
+    static int numeroDos = 20;
+    public static void main(String[] args) {
+        if (numeroUno > numeroDos) {
+            System.out.println(numeroUno + "es mayor que " + numeroDos);
+        }else{
+               System.out.println(numeroDos + "es mayor que" + numeroUno);
+        }
+          }
+}
